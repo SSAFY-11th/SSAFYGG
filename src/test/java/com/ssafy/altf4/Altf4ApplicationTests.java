cd71961ece4d9f6e05ba4ebeb43e7d7902755b40
Jinwoo <zvzv9808@gmail.com>
2024-03-11 21:19:22 +0900

    Initial Commit

---
+++ src/test/java/com/ssafy/altf4/Altf4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.altf4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Altf4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
