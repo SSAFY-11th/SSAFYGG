eeb3d58172fe6d39d1d1accc47a074fef9613393
Jinwoo <zvzv9808@gmail.com>
2024-03-13 19:33:28 +0900

    clean push

--- src/main/java/com/ssafy/altf4/entity/Member.java
+++ src/main/java/com/ssafy/altf4/entity/Member.java
@@ -1,2 +1,31 @@
-package com.ssafy.altf4.entity;public class Member {
+package com.ssafy.altf4.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @Builder
+    public Member(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
--- src/main/java/com/ssafy/altf4/repository/MemberRepository.java
+++ src/main/java/com/ssafy/altf4/repository/MemberRepository.java
@@ -1,2 +1,8 @@
-package com.ssafy.altf4.repository;public interface MemberRepository {
+package com.ssafy.altf4.repository;
+
+import com.ssafy.altf4.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Long, Member> {
+
 }
